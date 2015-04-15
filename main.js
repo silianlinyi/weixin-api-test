51881abfc5a0cad4776050ea47a8049505cddeff
KK <kk@qima-inc.com>
2015-04-15 21:45:56 +0800

    update

--- main.js
+++ main.js
@@ -166,7 +166,71 @@ weixin.on('VIEWEventMsg', function(data) {
     console.log(data);
 });
 
+// weixin.sendCustomMsg({
+//     'touser': 'ojim5txO8ivc0Ff2LKW1nlUJ9hM4',
+//     'msgtype': 'text',
+//     'text': {
+//         'content': 'Hello world'
+//     }
+// });
 
+// 自定义菜单查询接口
+// weixin.getMenu(function(data) {
+// console.log(data);
+// });
+
+// 自定义菜单删除接口
+// weixin.deleteMenu(function(data) {
+// console.log(data);
+// });
+
+// 自定义菜单创建接口
+var menuObj = {
+    "button": [{
+        "name": "扫码",
+        "sub_button": [{
+            "type": "scancode_waitmsg",
+            "name": "扫码带提示",
+            "key": "rselfmenu_0_0",
+            "sub_button": []
+        }, {
+            "type": "scancode_push",
+            "name": "扫码推事件",
+            "key": "rselfmenu_0_1",
+            "sub_button": []
+        }]
+    }, {
+        "name": "发图",
+        "sub_button": [{
+            "type": "pic_sysphoto",
+            "name": "系统拍照发图",
+            "key": "rselfmenu_1_0",
+            "sub_button": []
+        }, {
+            "type": "pic_photo_or_album",
+            "name": "拍照或者相册发图",
+            "key": "rselfmenu_1_1",
+            "sub_button": []
+        }, {
+            "type": "pic_weixin",
+            "name": "微信相册发图",
+            "key": "rselfmenu_1_2",
+            "sub_button": []
+        }]
+    }, {
+        "name": "发送位置",
+        "type": "location_select",
+        "key": "rselfmenu_2_0"
+    }]
+};
+// weixin.createMenu(menuObj, function(data) {
+//     console.log(data);
+// });
+
+// 获取自定义菜单配置接口
+// weixin.getCurrentSelfMenuInfo(function(data) {
+//     console.log(data);
+// });
 
 
 
